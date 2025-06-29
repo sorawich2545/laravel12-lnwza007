ff2240ef3a985fd17b83dfef3fec499a2b2c942a
sorawich2545 <marque086@hotmail.com>
2025-06-29 14:32:54 +0700

    Update index.blade.php

--- resources/views/test/index.blade.php
+++ resources/views/test/index.blade.php
@@ -1,4 +1,12 @@
-<h1>Sorwaich Sudamart 67222420009</h1>
+<!DOCTYPE html>
+<html>
+
+<body>
+    <h1>Sorwaich Sudamart 67222420009</h1>
     <a href="/gallery/ant"><img src="{{$ant}}"/></a> 
     <a href="/gallery/bird"><img src="{{$bird}}"/></a> 
-    <a href="/gallery/cat"><img src="{{$cat}}"/></a>
+    <a href="/gallery/cat"><img src="{{$cat}}"/></a> 
+    <a href="/gallery/ant"></a> 
+    <a href="/gallery/ant"></a> 
+</body>
+</html>
