2588abec012ada7fff02efa2571a0eb76d42c7d1
sorawich2545 <marque086@hotmail.com>
2025-07-06 15:31:59 +0700

    week03

--- routes/web.php
+++ routes/web.php
@@ -30,3 +30,46 @@ Route::get("/gallery/cat", function () {
 
 require __DIR__ . '/settings.php';
 require __DIR__ . '/auth.php';
+
+
+Route::get("/teacher", function () {
+    return view("teacher");
+});
+
+Route::get("/student", function () {
+    return view("student");
+});
+
+Route::get("/theme", function () {
+    return view("theme");
+});
+
+Route::get('/active/index', function () {
+    return view('active/index');
+})->name('index');
+
+Route::get('/active/about', function () {
+    return view('active/about');
+})->name('about');
+
+Route::get('/active/services', function () {
+    return view('active/services');
+})->name('services');
+
+Route::get('/active/portfolio', function () {
+    return view('active/portfolio');
+})->name('portfolio');
+
+Route::get('/active/team', function () {
+    return view('active/team');
+})->name('team');
+
+Route::get('/active/blog', function () {
+    return view('active/blog');
+})->name('blog');
+
+Route::get('/active/contact', function () {
+    return view('active/contact');
+})->name('contact');
+
+
